c9ce0926e795ff566782c275db65b7e97400b903
Sebastian <sebyzy@gmail.com>
2022-12-23 14:03:13 -0500

    12.23

--- draft.py
+++ draft.py
@@ -2,9 +2,12 @@ import requests
 import smtplib
 
 
-api_file_name = ""
+api_file_name = "api.txt"
 #API key
 api_file = open(api_file_name, "r")
 api_key = api_file.read()
+print(api_key)
 api_file.close()
 
+
+# home address input 
