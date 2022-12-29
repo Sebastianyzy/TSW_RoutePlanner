06d50d6af49edef6ce7d02dfc4677f37aeb99230
Sebastian <sebyzy@gmail.com>
2022-12-29 09:50:32 -0500

    Added gitignore file

--- RoutePlanner.py
+++ RoutePlanner.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-#Motive Token
-TOKEN = ""
+# Motive Token
+TOKEN = "Token.txt"
 api_prefix = "https://api.keeptruckin.com/api/i1/dispatch_share?token="
 googlemap_prefix = "https://www.google.com/maps/dir"
 
@@ -10,11 +10,12 @@ googlemap_prefix = "https://www.google.com/maps/dir"
 def get_token(TOKEN):
     file = open(TOKEN, "r")
     token = file.read()
-    token = str(token)
     file.close()
     return str(token)
 
-#concatenate the googlemap link with all the locations in trip list
+# concatenate the googlemap link with all the locations in trip list
+
+
 def generate_route_plan(gm_prefix, trip):
     i = 0
     url = gm_prefix
@@ -23,24 +24,26 @@ def generate_route_plan(gm_prefix, trip):
         i += 1
     return str(url)
 
-#extract token from a link
+# extract token from a link
+
+
 def generate_api_link(url):
     splitted = url.split("token=")
     return api_prefix+splitted[1]
 
 
 def load_trip(motive_link):
-    headers = {'X-Internal-Api-Key': TOKEN}  # get_token(TOKEN)
+    headers = {'X-Internal-Api-Key': get_token(TOKEN)}  # TOKEN
     data = requests.get(generate_api_link(motive_link), headers=headers)
     data = data.text
-    #use a list to store all addresses
+    # use a list to store all addresses
     trip = []
     parse_json = json.loads(data)
-    #start
+    # start
     consignee_dispatch_location = parse_json["dispatch"]["consignee_dispatch_location"]
-    #first stop
+    # first stop
     shipper_dispatch_location = parse_json["dispatch"]["shipper_dispatch_location"]
-    #the rest of the stops
+    # the rest of the stops
     dispatch_stops = parse_json["dispatch"]["dispatch_stops"]
     consignee = consignee_dispatch_location["address1"]+", "+consignee_dispatch_location["city"]+", " + \
         consignee_dispatch_location["state"]+" "+consignee_dispatch_location["zip"] + \
@@ -55,22 +58,20 @@ def load_trip(motive_link):
         trip.append(dispatch_stops[i]["dispatch_stop"]["dispatch_location"]["address1"]+", "+dispatch_stops[i]["dispatch_stop"]["dispatch_location"]["city"]+", " + dispatch_stops[i]["dispatch_stop"]
                     ["dispatch_location"]["state"]+" "+dispatch_stops[i]["dispatch_stop"]["dispatch_location"]["zip"] + ", " + dispatch_stops[i]["dispatch_stop"]["dispatch_location"]["country"])
         i += 1
-    #final destination is the origin 
+    # final destination is the origin
     trip.append(consignee)
     return trip
 
 
-
 def convert_hyperlink(uri, label=None):
-    if label is None: 
+    if label is None:
         label = uri
     parameters = ''
 
-    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST 
+    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST
     escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
 
     return escape_mask.format(parameters, uri, label)
-
 
 
 def main():
