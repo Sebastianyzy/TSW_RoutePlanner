5269114fe167246eb51a4fc5507c682c36d46903
Sebastian <sebyzy@gmail.com>
2023-01-19 16:13:59 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import sys
 import subprocess
 
-#pip install packages
+# pip install packages
+
+
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
-install('requests')
-install('selenium')
-install('webdriver_manager')
+def main():
+    install('requests')
+    install('selenium')
+    install('webdriver_manager')
+
+
+if __name__ == "__main__":
+    main()
